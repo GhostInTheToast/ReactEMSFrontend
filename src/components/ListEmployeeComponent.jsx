3f3a00380ea6509361f18026f28ba5e2dac0833c
GhostInTheToast <bmood1@gmail.com>
2025-02-04 19:21:08 -0600

    Rough frontend for employee list complete. React component for ListEmployee done.

---
+++ src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const ListEmployeeComponent = () => {
+
+const dummyData = [
+    {
+        "id":       1,
+        "firstName":"Billy",
+        "lastName": "Bobby",
+        "email":    "billybobby@email.com"
+    },
+    {
+        "id":       2,
+        "firstName":"Jacob",
+        "lastName": "Angy",
+        "email":    "Jacobangy@email.com"
+    },
+    {
+        "id":       3,
+        "firstName":"Sharon",
+        "lastName": "McCullen",
+        "email":    "Sharon2@email.com"
+    }
+]
+
+  return (
+    <div>
+
+        <h2>List of Employees</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Employee Id</th>
+                    <th>Employee First Name</th>
+                    <th>Employee Last Name</th>
+                    <th>Employee Email Id</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    dummyData.map(employee =>
+                        <tr key={employee.id}>
+                            <td>{employee.id}</td>
+                            <td>{employee.firstName}</td>
+                            <td>{employee.lastName}</td>
+                            <td>{employee.email}</td>
+                        </tr>
+                    )
+                }
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default ListEmployeeComponent
